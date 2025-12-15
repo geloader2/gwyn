0c7fb25390072fb0da25d4c0d36de34daf167682
GeLoader <geloader1@gmail.com>
2025-12-15 11:46:08 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/gwyn",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
